71783f4fefe3a937f0b5389e2c42f51682841461
Matthew Mueller <mattmuelle@gmail.com>
2014-09-01 02:51:46 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,65 @@
+
+# title-capitalization
+
+  Properly capitalize English titles.
+
+## Example
+
+```js
+capitalize('smashed, squashed, splattered, chewed, chunked and spewed')
+// Smashed, Squashed, Splattered, Chewed, Chunked and Spewed
+
+capitalize('capitalize('so long, and thanks for all the fish (hitchhiker\'s guide, #4)')
+// So Long, and Thanks for All the Fish (Hitchhiker's Guide, #4)
+```
+
+## Installation
+
+Node:
+
+```bash
+$ npm install title-capitalization
+```
+
+Browser (with [Duo](https://github.com/duojs/duo)):
+
+```js
+var capitalize = require('matthewmueller/title-capitalization');
+```
+
+## Credits
+
+This source has been adapted from http://titlecapitalization.com/.
+
+```
+* Cleaned up and added to NPM - Matthew Mueller - 1 Sept 2014
+* Extended Jan 12 2013 to lowercase all prepositions.
+* Ported to JavaScript By John Resig - http://ejohn.org/ - 21 May 2008
+* Original by John Gruber - http://daringfireball.net/ - 10 May 2008
+* License: http://www.opensource.org/licenses/mit-license.php
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2014 Matthew Mueller &lt;mattmuelle@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,113 @@
+/*
+ * Title Caps
+ *
+ * Cleaned up and added to NPM - Matthew Mueller - 1 Sept 2014
+ * Extended Jan 12 2013 to lowercase all prepositions.
+ * Ported to JavaScript By John Resig - http://ejohn.org/ - 21 May 2008
+ * Original by John Gruber - http://daringfireball.net/ - 10 May 2008
+ * License: http://www.opensource.org/licenses/mit-license.php
+ */
+
+
+/**
+ * Module Dependencies
+ */
+
+var prepositions = require('./lib/prepositions.json');
+var conjunctions = require('./lib/conjunctions.json');
+var articles = require('./lib/articles.json');
+
+/**
+ * Export `capitalize`
+ */
+
+module.exports = capitalize;
+
+/**
+ * Regexps
+ */
+
+var rpunct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
+var rlowercase = '(' + (prepositions.concat(articles).concat(conjunctions)).join('|') + ')';
+var rsplit = /[:.;?!] |(?: |^)["Ò]/g;
+
+/**
+ * I have no idea what these do :-P
+ */
+
+var ra = /[\u2018\u2019]/g;
+var rb = /[\u201C\u201D]/g;
+var rc = /\b([A-Za-z][a-z.'Õ]*)\b/g;
+var rd = /[A-Za-z]\.[A-Za-z]/;
+var re = RegExp("\\b" + rlowercase + "\\b", "ig")
+var rf = RegExp("^" + rpunct + rlowercase + "\\b", "ig");
+var rg = RegExp("\\b" + rlowercase + rpunct + "$", "ig");
+var rh = / V(s?)\. /ig;
+var ri = /(['Õ])S\b/ig;
+var rj = /\b(AT&T|Q&A)\b/ig;
+
+/**
+ * Properly capitalize a title
+ *
+ * @param {String} title
+ * @return {String} title
+ * @api public
+ */
+
+function capitalize(title) {
+  var parts = [];
+  var index = 0;
+
+  title = title
+    .replace(ra, "'")
+    .replace(rb, '"');
+
+  while (1) {
+    var m = rsplit.exec(title);
+
+    parts.push(title.substring(index, m ? m.index : title.length)
+      .replace(rc, function(all){
+        return rd.test(all) ? all : upper(all);
+      })
+      .replace(re, lower)
+      .replace(rf, function(all, punct, word){
+        return punct + upper(word);
+      })
+      .replace(rg, upper));
+
+    index = rsplit.lastIndex;
+
+    if (m) parts.push( m[0] );
+    else break;
+  }
+
+  return parts.join("").replace(rh, " v$1. ")
+    .replace(ri, "$1s")
+    .replace(rj, function(all){
+      return all.toUpperCase();
+    });
+}
+
+/**
+ * To lowercase
+ *
+ * @param {String} word
+ * @return {String}
+ * @api private
+ */
+
+function lower(word) {
+  return word.toLowerCase();
+}
+
+/**
+ * Capitalize
+ *
+ * @param {String} word
+ * @return {String}
+ * @api private
+ */
+
+function upper(word) {
+  return word.substr(0,1).toUpperCase() + word.substr(1);
+}
